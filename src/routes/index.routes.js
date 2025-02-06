166d2cd702207e2e7e5fcf80320b58857417dfa3
NeverVell <gavrielaldrixh@gmail.com>
2025-02-06 15:26:49 +0700

    Added user register route and some cleaner code todo: - Send user data to MYSQL

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,5 +4,6 @@ import userRoutes from "./user.routes.js"
 const router = Router();
 
 router.use("/users", userRoutes)
+// More route here such as cart, orders, etc
 
 export default router
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,5 +4,6 @@ import userController from "../controller/user.controller.js";
 const router = Router();
 
 router.post("/register", userController.register);
+// More route here such as login, logout, userprofile, etc.
 
 export default router;
